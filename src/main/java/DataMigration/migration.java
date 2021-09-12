9b1bfff21189b2c98618fc34519410a00ae18a94
Komol Kunty Rajib <rajib2@qmail.com.bd>
2021-09-12 19:43:20 +0600

    Migration script complete

--- src/main/java/DataMigration/migration.java
+++ src/main/java/DataMigration/migration.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class migration {
 
@@ -25,6 +26,7 @@ public class migration {
         String db_data_type;
         Connection connection = DatabaseConnection.connect();
         Statement statement = null;
+        Boolean match_id = null;
 
         try {
             statement = connection.createStatement();
@@ -33,16 +35,21 @@ public class migration {
         }
         assert statement != null;
 
-        try (BufferedReader br = Files.newBufferedReader(Paths.get("/home/rajib/Documents/rajib/ewu/Summer 2021/cse110/live-cricket-scorecard/dataset/archive/IPL Matches 2008-2020.csv"))) {
+        System.out.println("Enter the table name: ");
+        Scanner sc = new Scanner(System.in);
+        String table_name = sc.next();
+        sc.nextLine();
+
+        System.out.println("Enter the CSV file path: ");
+        String filepath = sc.nextLine();
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filepath))) {
             String[] first_line = br.readLine().split(",");
             String[] second_line = br.readLine().split(",", -1);
-            StringBuilder sql_query = new StringBuilder();
+            StringBuilder sql_query_ct = new StringBuilder();
+            StringBuilder sql_query_cd = new StringBuilder();
 
-            System.out.println("Enter the table name: ");
-            Scanner sc = new Scanner(System.in);
-            String table_name = sc.next();
-
-            sql_query.append("CREATE TABLE `").append(table_name).append("` (");
+            sql_query_ct.append("CREATE TABLE `").append("test").append("`.`").append(table_name).append("` (");
 
             for (int i = 0; i < first_line.length; i++) {
                 first_line[i] = first_line[i].replace("\"", "");
@@ -56,15 +63,53 @@ public class migration {
 
                 if (Objects.equals(first_line[i], "id")) {
                     first_line[i] = "match_id";
+                    match_id = true;
+                } else {
+                    match_id = false;
                 }
 
-                sql_query.append("`").append(first_line[i]).append("`").append(" ").append(db_data_type).append(", ");
+                sql_query_ct.append("`").append(first_line[i]).append("`").append(" ").append(db_data_type).append(", ");
             }
 
-            sql_query.append(")");
-            sql_query = new StringBuilder(String.valueOf(sql_query).replace(", )", ");"));
+            sql_query_ct.append(" PRIMARY KEY (`match_id`))").append(" ENGINE = InnoDB;");
+            statement.executeUpdate(String.valueOf(sql_query_ct));
 
-            statement.executeUpdate(String.valueOf(sql_query));
+            sql_query_cd.append("INSERT INTO `").append(table_name).append("`(");
+
+            for (String s : first_line) {
+                sql_query_cd.append("`").append(s).append("`").append(", ");
+            }
+
+            sql_query_cd.append(")");
+            sql_query_cd = new StringBuilder(String.valueOf(sql_query_cd).replace(", )", ") VALUES ("));
+            StringBuilder we = new StringBuilder();
+
+            StringBuilder old = sql_query_cd;
+            sql_query_cd = new StringBuilder(String.valueOf(sql_query_cd));
+
+
+            for (String s : second_line) {
+                we.append("'").append(s).append("',");
+            }
+
+            sql_query_cd.append(we).append(")");
+            statement.executeUpdate(String.valueOf(sql_query_cd).replace(",)", ")") + ";");
+
+            we = new StringBuilder();
+            sql_query_cd = new StringBuilder(old);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.replace("\"", "").replace(", ", "-").replace('\'', '`');
+                for (String s : line.split(",", -1)) {
+                    we.append("'").append(s).append("',");
+                }
+
+                sql_query_cd.append(we.append(");"));
+                statement.executeUpdate(String.valueOf(sql_query_cd).replace(",);", ");"));
+                we = new StringBuilder();
+                sql_query_cd = new StringBuilder(old);
+            }
         }
     }
 }
