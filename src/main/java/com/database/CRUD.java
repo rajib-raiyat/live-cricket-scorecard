a859fb9d4d8bdfb19f33fd4e8a8e03a0fc53942c
Komol Kunty Rajib <rajib2@qmail.com.bd>
2021-09-13 16:14:17 +0600

    Get match details by ID method added

--- src/main/java/com/database/CRUD.java
+++ src/main/java/com/database/CRUD.java
@@ -1,8 +1,11 @@
 package com.database;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class CRUD {
     private static final Connection connection = DatabaseConnection.connect();
@@ -22,16 +25,25 @@ public class CRUD {
     }
 
     public static void update() {
-
     }
 
-    public static void get() {
+    public static Dictionary<String, String> getMatchDetailsByID(String match_id) throws SQLException {
+        Dictionary<String, String> dataset = new Hashtable<>();
 
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM ipl_matches WHERE match_id=" + match_id + ";");
+        int count = resultSet.getMetaData().getColumnCount();
+
+        while (resultSet.next()) {
+            for (int i = 1; i <= count; i++) {
+                String column_name = resultSet.getMetaData().getColumnName(i);
+                String value = resultSet.getString(column_name);
+                dataset.put(column_name, value);
+            }
+        }
+
+        return dataset;
     }
 
     public static void delete() {
-
     }
-
-
 }
--- src/main/java/com/database/DatabaseDriver.java
+++ src/main/java/com/database/DatabaseDriver.java
@@ -1,11 +1,16 @@
 package com.database;
 
 import java.sql.SQLException;
+import java.util.Dictionary;
 
 public class DatabaseDriver {
     public static void main(String[] args) throws SQLException {
+        Dictionary<String, String> dataset = CRUD.getMatchDetailsByID("120");
 
-        String query = "INSERT INTO student VALUES ('2018-2-60-22', 'sadat')";
-        CRUD.create(query);
+        if (dataset.isEmpty()) {
+            System.out.println("data not found.");
+        } else {
+            System.out.println(dataset);
+        }
     }
 }
